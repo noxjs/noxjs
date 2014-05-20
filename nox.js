921715484b7e2a563e9ca1bf404e6bcbab5ae211
Mauricio Soares <m.soares2603@gmail.com>
2014-05-20 19:48:29 -0300

    working with namespaces

--- nox.js
+++ nox.js
@@ -3,37 +3,57 @@
 		var args = Array.prototype.slice.call(arguments),
 
 			// first arg is the namespace
-			namespace = args.shift(),
+			ns_string = args.shift(),
 
 			// last arg is the callback
 			callback = args.pop(),
 
 			// gets all modules beeing passed as different args, or as an Array
 			modules = (args[0] && typeof args[0] === 'string') ? args : args[0],
-			i,
 			dependencies = {};
-
-			// if its not beeing called as a constructor
-			// if(!(this instanceof Nox)) {
-			// 	return new Nox(modules, callback);
-			// }
 
 			// if no module is passed, or '*'
 			if(!modules || modules[0] === '*') {
 				modules = [];
-				for(i in Nox.modules) {
+				for(var i in Nox.modules) {
 					modules.push(i);
 				}
 			}
 
 			// starts all the modules
-			for(i = 0; i < modules.length; i += 1) {
+			for(var i = 0; i < modules.length; i += 1) {
 				Nox.modules[modules[i]](dependencies);
 			}
 
-			// calls the callback
-			return new callback(dependencies);
+			// adds the callback to the namespace
+			a = namespace(ns_string);
+			// new callback(dependencies);
+
 	};
+
+	var namespace = function(ns_string) {
+		var parts = ns_string.split('.'),
+			parent = global,
+			i;
+
+		for(i = 0; i < parts.length; i += 1) {
+			if(typeof parent[parts[i]] === 'undefined') {
+				parent[parts[i]] = {};
+			}
+
+			parent = parent[parts[i]];
+		}
+
+		return parent;
+	}
+
+	global.Nox = Nox;
+
+} (window));
+
+// modules
+(function(global) {
+	var Nox = global.Nox;
 
 	Nox.modules = {};
 	Nox.modules.dom = function(box) {
@@ -53,7 +73,4 @@
 			click: function() {}
 		}
 	}
-
-	global.Nox = Nox;
-
 } (window));
