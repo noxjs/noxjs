2399673e5565627146c5e3be7124da7b09d0ba2b
Mauricio Soares <m.soares2603@gmail.com>
2014-05-20 19:17:58 -0300

    wrap everything in a IIFE

--- nox.js
+++ nox.js
@@ -1,63 +1,59 @@
-function Sandbox() {
-	var args = Array.prototype.slice.call(arguments),
+(function(global) {
+	var Nox = function() {
+		var args = Array.prototype.slice.call(arguments),
 
-		// last arg is the callback
-		callback = args.pop(),
+			// first arg is the namespace
+			namespace = args.shift(),
 
-		// gets all modules beeing passed as different args, or as an Array
-		modules = (args[0] && typeof args[0] === 'string') ? args : args[0],
-		i,
-		dependencies = {};
+			// last arg is the callback
+			callback = args.pop(),
 
-		// if its not beeing called as a constructor
-		// if(!(this instanceof Sandbox)) {
-		// 	return new Sandbox(modules, callback);
-		// }
+			// gets all modules beeing passed as different args, or as an Array
+			modules = (args[0] && typeof args[0] === 'string') ? args : args[0],
+			i,
+			dependencies = {};
 
-		// if no module is passed, or '*'
-		if(!modules || modules[0] === '*') {
-			modules = [];
-			for(i in Sandbox.modules) {
-				modules.push(i);
+			// if its not beeing called as a constructor
+			// if(!(this instanceof Nox)) {
+			// 	return new Nox(modules, callback);
+			// }
+
+			// if no module is passed, or '*'
+			if(!modules || modules[0] === '*') {
+				modules = [];
+				for(i in Nox.modules) {
+					modules.push(i);
+				}
 			}
+
+			// starts all the modules
+			for(i = 0; i < modules.length; i += 1) {
+				Nox.modules[modules[i]](dependencies);
+			}
+
+			// calls the callback
+			return new callback(dependencies);
+	};
+
+	Nox.modules = {};
+	Nox.modules.dom = function(box) {
+		box.dom = {
+			getElement: function() {}
 		}
+	}
 
-		// starts all the modules
-		for(i = 0; i < modules.length; i += 1) {
-			Sandbox.modules[modules[i]](dependencies);
+	Nox.modules.ajax = function(box) {
+		box.ajax = {
+			request: function() {}
 		}
-
-		// calls the callback
-		return new callback(dependencies)
-}
-
-Sandbox.modules = {};
-Sandbox.modules.dom = function(box) {
-	box.dom = {
-		getElement: function() {}
 	}
-}
 
-Sandbox.modules.ajax = function(box) {
-	box.ajax = {
-		request: function() {}
+	Nox.modules.events = function(box) {
+		box.events = {
+			click: function() {}
+		}
 	}
-}
 
-Sandbox.modules.events = function(box) {
-	box.events = {
-		click: function() {}
-	}
-}
+	global.Nox = Nox;
 
-var View = Sandbox('*', function(box) {
-	this.initialize = function() {
-		console.log(box);
-	}
-});
-
-var Home = Sandbox(['dom', 'ajax'], function(box) {
-	this.initialize = function() {
-		console.log(box);
-	}
-});
+} (window));
