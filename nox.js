88c655723838a4a8b4fcb6b73d98a7991e6932d6
Mauricio Soares <m.soares2603@gmail.com>
2014-05-21 00:48:51 -0300

    working great with namespaces

--- nox.js
+++ nox.js
@@ -1,4 +1,13 @@
+/**
+* Nox.js 0.0.0
+*
+* Created by: Mauricio Soares (http://github.com/msodeveloper)
+* Inspired by the Sandbox pattern
+*/
+
 (function(global) {
+  'use strict';
+
 	var Nox = function() {
 		var args = Array.prototype.slice.call(arguments),
 
@@ -8,69 +17,87 @@
 			// last arg is the callback
 			callback = args.pop(),
 
-			// gets all modules beeing passed as different args, or as an Array
-			modules = (args[0] && typeof args[0] === 'string') ? args : args[0],
-			dependencies = {};
+      // defines a dependencie object, where all
+      // dependencies are stored to pass in the function
+			dependencies = {},
 
-			// if no module is passed, or '*'
-			if(!modules || modules[0] === '*') {
-				modules = [];
-				for(var i in Nox.modules) {
-					modules.push(i);
-				}
+      // all modules from the args will be stored in here
+      modules;
+
+    // gets all modules beeing passed as different args, or as an Array
+    if(args[0] && typeof args[0] === 'string') {
+      modules = args;
+    } else if(args[0] && typeof args[0] === 'object') {
+      modules = args[0];
+    } else {
+      modules = [];
+    }
+
+
+		// if no module is passed, or '*' is passed
+    // gets all modules
+    if(modules && modules[0] === '*') {
+      modules = [];
+			for(var i in Nox.modules) {
+				modules.push(i);
 			}
+		}
 
-			// starts all the modules
-			for(var i = 0; i < modules.length; i += 1) {
-				Nox.modules[modules[i]](dependencies);
-			}
+		// starts all the modules
+		for(var i = 0; i < modules.length; i += 1) {
+			Nox.modules[modules[i]](dependencies);
+		}
 
-			// adds the callback to the namespace
-			a = namespace(ns_string);
-			// new callback(dependencies);
-
+		// adds the callback to the namespace
+		namespace(ns_string, {
+      callback: callback,
+      dependencies: dependencies
+    });
 	};
 
-	var namespace = function(ns_string) {
+	var namespace = function(ns_string, conf) {
 		var parts = ns_string.split('.'),
 			parent = global,
+      length = parts.length,
 			i;
 
-		for(i = 0; i < parts.length; i += 1) {
-			if(typeof parent[parts[i]] === 'undefined') {
+		for(i = 0; i < length; i += 1) {
+      if(i + 1 === length) {
+        return parent[parts[i]] = new conf.callback(conf.dependencies);
+      } else if(typeof parent[parts[i]] === 'undefined') {
 				parent[parts[i]] = {};
 			}
 
 			parent = parent[parts[i]];
 		}
-
-		return parent;
-	}
+	};
 
 	global.Nox = Nox;
 
-} (window));
+} (this));
 
 // modules
 (function(global) {
+  'use strict';
+
 	var Nox = global.Nox;
 
 	Nox.modules = {};
 	Nox.modules.dom = function(box) {
 		box.dom = {
 			getElement: function() {}
-		}
-	}
+		};
+	};
 
 	Nox.modules.ajax = function(box) {
 		box.ajax = {
 			request: function() {}
-		}
-	}
+		};
+	};
 
 	Nox.modules.events = function(box) {
 		box.events = {
 			click: function() {}
-		}
-	}
-} (window));
+		};
+	};
+} (this));
