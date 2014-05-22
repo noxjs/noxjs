9d24a0abf91f099b9b12bc4e319b05f737f96c64
Mauricio Soares <m.soares2603@gmail.com>
2014-05-22 19:02:33 -0300

    change order of function beeing called

--- dist/nox.js
+++ dist/nox.js
@@ -1,6 +1,6 @@
 /** nox.js - v0.0.0 - 2014-05-22
 * Copyright (c) 2014 Mauricio Soares de Oliveira;
-* Licensed MIT 
+* Licensed MIT
 */
 
 /**
@@ -20,21 +20,18 @@
       // will be invoked as a class with "new"
       Callback = Nox.methods.getCallback(args),
 
+      // gets all modules beeing passed as different args, or as an Array
+      modules = Nox.methods.getModules(args),
+
       // defines a dependencies object, where all
       // dependencies are stored to pass in the callback
       dependencies = {},
-
-      // all modules from the args will be stored in here
-      modules,
 
       // the function after aliased and with modules
       fn,
 
       // used in loopings
       i;
-
-    // gets all modules beeing passed as different args, or as an Array
-    modules = Nox.methods.getModules(args);
 
     // starts all the modules
     for(i = 0; i < modules.length; i += 1) {
