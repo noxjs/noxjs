4f33751b64fdc0d9780b227a0b1536a320386045
Mauricio Soares <m.soares2603@gmail.com>
2014-05-23 16:38:01 -0300

    update comments in module

--- src/core/module.js
+++ src/core/module.js
@@ -1,6 +1,14 @@
 (function(Nox) {
   'use strict';
 
+  /**
+  * Includes a new module into the modules index of Nox
+  *
+  * @method module
+  * @param {String} index Basically the name of the module
+  * @param {Function} fn the module itself
+  */
+
   Nox.module = function(index, fn) {
     Nox.modules = Nox.modules || {};
 
