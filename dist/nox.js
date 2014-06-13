e28ad47ae499b3fe5b463d412e9e11fcc8f2994f
Mauricio Soares <m.soares2603@gmail.com>
2014-06-13 15:42:32 -0300

    rename variables, add comments

--- dist/nox.js
+++ dist/nox.js
@@ -1,4 +1,4 @@
-/** nox.js - v0.1.0 - 2014-06-13
+/** nox.js - v0.0.8 - 2014-06-13
 * Copyright (c) 2014 Mauricio Soares de Oliveira;
 * Licensed MIT 
 */
@@ -95,7 +95,16 @@
 (function() {
   'use strict';
 
-  Nox.decorator = function(ns_string, fn) {
+  /**
+  * Includes a new decorate into the decorator of the constructor
+  *
+  * @method decorator
+  * @param {String} ns_string name of the module and decorator splited by an underline,
+  * example: 'App.Home_mydecorator'
+  * @param {Object} obj the new object that the constructor will receive
+  */
+
+  Nox.decorator = function(ns_string, obj) {
     var namespace = Nox.methods.namespace(ns_string),
     decorator_string,
     constr;
@@ -110,7 +119,7 @@
     constr.decorators = constr.decorators || {};
 
     // adds the index of new decorator
-    constr.decorators[decorator_string[1]] = fn;
+    constr.decorators[decorator_string[1]] = obj;
   };
 } ());
 /**
--- src/core/decorator.js
+++ src/core/decorator.js
@@ -1,7 +1,16 @@
 (function() {
   'use strict';
 
-  Nox.decorator = function(ns_string, fn) {
+  /**
+  * Includes a new decorate into the decorator of the constructor
+  *
+  * @method decorator
+  * @param {String} ns_string name of the module and decorator splited by an underline,
+  * example: 'App.Home_mydecorator'
+  * @param {Object} obj the new object that the constructor will receive
+  */
+
+  Nox.decorator = function(ns_string, obj) {
     var namespace = Nox.methods.namespace(ns_string),
     decorator_string,
     constr;
@@ -16,6 +25,6 @@
     constr.decorators = constr.decorators || {};
 
     // adds the index of new decorator
-    constr.decorators[decorator_string[1]] = fn;
+    constr.decorators[decorator_string[1]] = obj;
   };
 } ());
