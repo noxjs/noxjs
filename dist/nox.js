d1c359aa5c8d360c5ad00b49907c0eee680d5261
Mauricio Soares <m.soares2603@gmail.com>
2014-06-15 18:07:13 -0300

    major core updates, add emitter, update version

--- dist/nox.js
+++ dist/nox.js
@@ -1,4 +1,4 @@
-/** nox.js - v0.1.0 - 2014-06-13
+/** nox.js - v0.2.0 - 2014-06-15
 * Copyright (c) 2014 Mauricio Soares de Oliveira;
 * Licensed MIT 
 */
@@ -51,11 +51,12 @@
       // calls the callback with the function itself, and the dependencies
       callback.apply(this, newArgs);
 
+      Nox.methods.addDecorator(namespace.fn);
+      Nox.methods.addEmitter.call(this, namespace.fn);
+
       if(typeof namespace.fn.initialize === 'function') {
         namespace.fn.initialize.apply(this, slice.call(arguments));
       }
-
-      Nox.methods.addDecorator(namespace.fn);
     };
 
     // Adds the new constructor as a Nox Module
@@ -134,19 +135,114 @@
   Nox.methods = Nox.methods || {};
 
   Nox.methods.addDecorator = function(fn) {
-    fn.decorate = function(decorator) {
-      var overrides = this.constructor.decorators[decorator],
-      newobj,
-      i;
+    if(typeof fn.decorate !== 'function') {
+      fn.decorate = function(decorator) {
+        var overrides = this.constructor.decorators[decorator],
+        newobj,
+        i;
 
-      newobj = Object.create(this);
-      newobj.uber = this;
+        newobj = Object.create(this);
+        newobj.uber = this;
 
-      for(i in overrides) {
-        newobj[i] = overrides[i];
-      }
-      return newobj;
+        for(i in overrides) {
+          newobj[i] = overrides[i];
+        }
+        return newobj;
+      };
+    }
+  };
+} (this, this.Nox));
+
+/**
+* Adds the decorate method to the prototype, thx to this little guy:
+* https://gist.github.com/mudge/5830382
+*
+* @method addEmitter
+* @param {Object} fn the prototype of the constructor
+*/
+(function(global, Nox) {
+  'use strict';
+
+  Nox.methods = Nox.methods || {};
+
+  /* Polyfill indexOf. */
+  var indexOf;
+
+  if (typeof Array.prototype.indexOf === 'function') {
+    indexOf = function (haystack, needle) {
+      return haystack.indexOf(needle);
     };
+  } else {
+    indexOf = function (haystack, needle) {
+      var i = 0, length = haystack.length, idx = -1, found = false;
+
+      while (i < length && !found) {
+        if (haystack[i] === needle) {
+          idx = i;
+          found = true;
+        }
+
+        i++;
+      }
+
+      return idx;
+    };
+  }
+
+  Nox.methods.addEmitter = function(fn) {
+    this.events = {};
+
+    if(typeof fn.on !== 'function') {
+      fn.on = function(event, listener) {
+        if(typeof this.events[event] !== 'object') {
+          this.events[event] = [];
+        }
+
+        this.events[event].push(listener);
+      };
+    }
+
+    if(typeof fn.removeListener !== 'function') {
+      fn.removeListener = function(event, listener) {
+        var idx;
+
+        if(typeof this.events[event] === 'object') {
+          idx = indexOf(this.events[event], listener);
+
+          if(idx > -1) {
+            this.events[event].splice(idx, 1);
+          }
+        }
+      };
+    }
+
+    if(typeof fn.emit !== 'function') {
+      fn.emit = function(event) {
+        var i,
+          listeners,
+          length,
+          args = Array.prototype.slice.call(arguments, 1);
+
+        if(typeof this.events[event] === 'object') {
+          listeners = this.events[event].slice();
+          length = listeners.length;
+
+          for(i = 0; i < length; i += 1) {
+            listeners[i].apply(this, args);
+          }
+        }
+      };
+    }
+
+    if(typeof fn.once !== 'function') {
+      fn.once = function(event, listener) {
+        this.on(event, function g() {
+          this.removeListener(event, g);
+          listener.apply(this, arguments);
+        });
+      };
+    }
+
   };
 } (this, this.Nox));
 
