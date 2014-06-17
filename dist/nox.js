95c5f04a1b6f4f2ac7f596f70325753e1ad20bf2
Mauricio Soares <m.soares2603@gmail.com>
2014-06-17 19:03:43 -0300

    generate dist

--- dist/nox.js
+++ dist/nox.js
@@ -1,4 +1,4 @@
-/** nox.js - v0.2.3 - 2014-06-16
+/** nox.js - v0.2.3 - 2014-06-17
 * Copyright (c) 2014 Mauricio Soares de Oliveira;
 * Licensed MIT 
 */
@@ -165,30 +165,6 @@
 
   Nox.methods = Nox.methods || {};
 
-  /* Polyfill indexOf. */
-  var indexOf;
-
-  if (typeof Array.prototype.indexOf === 'function') {
-    indexOf = function (haystack, needle) {
-      return haystack.indexOf(needle);
-    };
-  } else {
-    indexOf = function (haystack, needle) {
-      var i = 0, length = haystack.length, idx = -1, found = false;
-
-      while (i < length && !found) {
-        if (haystack[i] === needle) {
-          idx = i;
-          found = true;
-        }
-
-        i++;
-      }
-
-      return idx;
-    };
-  }
-
   Nox.methods.addEmitter = function(fn) {
     this.events = {};
 
@@ -326,6 +302,45 @@
 
     return namespace.join('.');
   };
+} (this, this.Nox));
+
+/**
+* Adds the decorate method to the prototype, thx to this little guy:
+*
+* @method indexOf
+* @param {Array} haystack the array which will look for
+* @param {Object} needle the object i'm looking for
+* @return {Number} idx the index of the element, if
+* there's none, then return -1
+*/
+(function(global, Nox) {
+  'use strict';
+
+  Nox.methods = Nox.methods || {};
+
+  if (typeof Array.prototype.indexOf === 'function') {
+    Nox.methods.indexOf = function (haystack, needle) {
+      return haystack.indexOf(needle);
+    };
+  } else {
+    Nox.methods.indexOf = function (haystack, needle) {
+      var i = 0,
+        length = haystack.length,
+        idx = -1,
+        found = false;
+
+      while (i < length && !found) {
+        if (haystack[i] === needle) {
+          idx = i;
+          found = true;
+        }
+
+        i++;
+      }
+
+      return idx;
+    };
+  }
 } (this, this.Nox));
 
 /**
